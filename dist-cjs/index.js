bca9c1034d0a15451ec93ce5b17688651850fba5
srymh <31023018+srymh@users.noreply.github.com>
2020-10-09 00:50:00 +0900

    add dist

---
+++ dist-cjs/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+function hello() {
+    return 'hellos';
+}
+exports.hello = hello;
+//# sourceMappingURL=index.js.map
